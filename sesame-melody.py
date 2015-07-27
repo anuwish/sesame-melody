a30b52aecd5af648ee0528df3a21600a79002b52
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2015-07-27 21:35:23 +0200

    reduced the saple rate.

--- sesame-melody.py
+++ sesame-melody.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # Taken from http://flothesof.github.io/pyqt-microphone-fft-application.html
 class MicrophoneRecorder(object):
-    def __init__(self, rate=48000, chunksize=2048):
+    def __init__(self, rate=10000, chunksize=2048):
         self.rate = rate
         self.chunksize = chunksize
         self.p = pyaudio.PyAudio()
@@ -29,13 +29,13 @@ class MicrophoneRecorder(object):
             if self.stop:
                 return None, pyaudio.paComplete
         return None, pyaudio.paContinue
-    
+
     def get_frames(self):
         with self.lock:
             frames = self.frames
             self.frames = []
             return frames
-    
+
     def start(self):
         self.stream.start_stream()
 
@@ -60,19 +60,19 @@ class MplFigure(object):
 class LiveFFTWidget(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
-        
+
         # customize the UI
         self.initUI()
-        
+
         # init class data
-        self.initData()       
-        
+        self.initData()
+
         # connect slots
         self.connectSlots()
-        
+
         # init MPL widget
         self.initMplWidget()
-        
+
     def initUI(self):
 
         hbox_gain = QtGui.QHBoxLayout()
@@ -80,10 +80,10 @@ class LiveFFTWidget(QtGui.QWidget):
         autoGainCheckBox = QtGui.QCheckBox(checked=True)
         hbox_gain.addWidget(autoGain)
         hbox_gain.addWidget(autoGainCheckBox)
-        
+
         # reference to checkbox
         self.autoGainCheckBox = autoGainCheckBox
-        
+
         hbox_fixedGain = QtGui.QHBoxLayout()
         fixedGain = QtGui.QLabel('Manual gain level for frequency spectrum')
         fixedGainSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
@@ -101,38 +101,38 @@ class LiveFFTWidget(QtGui.QWidget):
         self.main_figure = MplFigure(self)
         vbox.addWidget(self.main_figure.toolbar)
         vbox.addWidget(self.main_figure.canvas)
-        
-        self.setLayout(vbox) 
-        
+
+        self.setLayout(vbox)
+
         self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('LiveFFT')    
+        self.setWindowTitle('LiveFFT')
         self.show()
         # timer for callbacks, taken from:
         # http://ralsina.me/weblog/posts/BB974.html
         timer = QtCore.QTimer()
         timer.timeout.connect(self.handleNewData)
         timer.start(100)
-        # keep reference to timer        
+        # keep reference to timer
         self.timer = timer
-        
-     
+
+
     def initData(self):
         mic = MicrophoneRecorder()
-        mic.start()  
+        mic.start()
 
-        # keeps reference to mic        
+        # keeps reference to mic
         self.mic = mic
-        
+
         # computes the parameters that will be used during plotting
-        self.freq_vect = np.fft.rfftfreq(mic.chunksize, 
+        self.freq_vect = np.fft.rfftfreq(mic.chunksize,
                                          1./mic.rate)
         self.time_vect = np.arange(mic.chunksize, dtype=np.float32) / mic.rate * 1000
-                
+
     def connectSlots(self):
         pass
-    
+
     def initMplWidget(self):
-        """creates initial matplotlib plots in the main window and keeps 
+        """creates initial matplotlib plots in the main window and keeps
         references for further use"""
         # top plot
         self.ax_top = self.main_figure.figure.add_subplot(211)
@@ -146,41 +146,39 @@ class LiveFFTWidget(QtGui.QWidget):
         #self.ax_bottom.set_xlim(0, self.freq_vect.max())
         self.ax_bottom.set_xlim(0, 4000)
         self.ax_bottom.set_xlabel(u'frequency (Hz)', fontsize=6)
-        # line objects        
-        self.line_top, = self.ax_top.plot(self.time_vect, 
+        # line objects
+        self.line_top, = self.ax_top.plot(self.time_vect,
                                          np.ones_like(self.time_vect))
-        
+
         self.line_bottom, = self.ax_bottom.plot(self.freq_vect,
                                                np.ones_like(self.freq_vect))
-                                               
 
-                                               
+
+
     def handleNewData(self):
-        """ handles the asynchroneously collected sound chunks """        
-        # gets the latest frames        
+        """ handles the asynchroneously collected sound chunks """
+        # gets the latest frames
         frames = self.mic.get_frames()
-        
+
         if len(frames) > 0:
             # keeps only the last frame
             current_frame = frames[-1]
             # plots the time signal
             self.line_top.set_data(self.time_vect, current_frame)
-            # computes and plots the fft signal            
+            # computes and plots the fft signal
             fft_frame = np.fft.rfft(current_frame)
             if self.autoGainCheckBox.checkState() == QtCore.Qt.Checked:
                 fft_frame /= np.abs(fft_frame).max()
             else:
                 fft_frame *= (1 + self.fixedGainSlider.value()) / 5000000.
                 #print(np.abs(fft_frame).max())
-            self.line_bottom.set_data(self.freq_vect, np.abs(fft_frame))            
-            
+            self.line_bottom.set_data(self.freq_vect, np.abs(fft_frame))
+
             # refreshes the plots
             self.main_figure.canvas.draw()
 
-import sys 
-if __name__ == "__main__": 
-    app = QtGui.QApplication(sys.argv) 
-    window = LiveFFTWidget() 
+import sys
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = LiveFFTWidget()
     sys.exit(app.exec_())
-
-
