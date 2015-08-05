a5208cb2cd16aec2aa671c223fbb81320283c0b5
Raspberry Pi <raspi@local>
2015-08-05 18:27:52 +0000

    servo: works now with hardware PWM using on RPIO python module

--- servo-hpwm-test.py
+++ servo-hpwm-test.py
@@ -1,10 +1,9 @@
-import RPi.GPIO as GPIO
 from RPIO import PWM
 import time
 import os
 
-# output pin for the PWM signal 
-PIN = 7
+# output GPIO for the PWM signal 
+NGPIO = 18
 
 # the servo is a 'TowerPro SG90' or similar
 # http://www.servodatabase.com/servo/towerpro/sg90
@@ -13,11 +12,9 @@ PIN = 7
 ANGLE_RANGE = 180 # tested, not for sure
 BASE_FREQ = 50    # user setting
 BASE_PERIOD = 1000.0 * 1/BASE_FREQ
-MIN_PULSE_WIDTH = 0.5
-MAX_PULSE_WIDTH = 2.4 
-MIN_POS = 100.0 * MIN_PULSE_WIDTH/BASE_PERIOD
-MAX_POS = 100.0 * MAX_PULSE_WIDTH/BASE_PERIOD
-NEUTRAL_POS = 0.5 * (MAX_POS + MIN_POS)
+MIN_PULSE_WIDTH = 500
+MAX_PULSE_WIDTH = 2400 
+NEUTRAL_WIDTH = 0.5 * (MAX_PULSE_WIDTH + MIN_PULSE_WIDTH)
 
 T_PER_60 = 0.17 # tested, not for sure
 T_MAX = ANGLE_RANGE/60.0 * T_PER_60
@@ -26,7 +23,7 @@ T_MAX = ANGLE_RANGE/60.0 * T_PER_60
 # thus, an initialisation value is necessary, took center position
 POS = 0.5 * ANGLE_RANGE
 
-print "angle range", ANGLE_RANGE, "base freq", BASE_FREQ, "base period", BASE_PERIOD, "min/max pulse width", MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, "min/max pos", MIN_POS, MAX_POS, "neutral pos", NEUTRAL_POS, "t per 60", T_PER_60, "initial pos", POS
+#print "angle range", ANGLE_RANGE, "base freq", BASE_FREQ, "base period", BASE_PERIOD, "min/max pulse width", MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, "min/max pos", "neutral width", NEUTRAL_WIDTH, "t per 60", T_PER_60, "initial pos", POS
 
 def set_POS(angle):
   global POS
@@ -35,62 +32,46 @@ def set_POS(angle):
 
 def pulse_length(angle):
   # return pulse length to reach requested angle
-  increment = (MAX_POS - MIN_POS) / ANGLE_RANGE
-  pl = MIN_POS + (increment * angle)
-  print "Pulse length is", pl
+  increment = (MAX_PULSE_WIDTH - MIN_PULSE_WIDTH) / ANGLE_RANGE
+  pl = MIN_PULSE_WIDTH + (increment * angle)
+  #print "Pulse length is", pl
   return pl
 
 def duration(angle):
   # calculate signal duration from current and final position
   diff = abs(POS - angle)
   d = T_PER_60/60.0 * diff
-  print "Duration is", d
+  #print "Duration is", d
   set_POS(angle)
   return d
 
 def init(servo):
   # initialise servo, move to inital POS
   print "Initialise servo at pos", POS
-  servo.start(pulse_length(POS))
+  servo.set_servo(NGPIO, pulse_length(POS))
   time.sleep(T_MAX)
-  servo.stop()
+  servo.stop_servo(NGPIO)
   time.sleep(1)
 
 def move(servo, angle):
   # move 'servo' to 'angle'
-  print "Move servo to", angle
-  servo.start(pulse_length(angle))
+  #print "Move servo to", angle
+  servo.set_servo(NGPIO, pulse_length(angle))
   time.sleep(duration(angle))
-  #time.sleep(T_MAX)
-  servo.stop()
-  time.sleep(0.5)
+  servo.stop_servo(NGPIO)
+  time.sleep(1)
 
 
 def main():
-  #GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(PIN, GPIO.OUT)
+  servo = PWM.Servo()
+  time.sleep(0.2)
+  
+  init(servo)
 
-  servo = GPIO.PWM(PIN, BASE_FREQ)
-  #init(servo)
- 
-  # manual
-  servo.start(pulse_length(180))
-  time.sleep(T_MAX)
-  servo.stop()
-  time.sleep(1)
-  servo.start(pulse_length(0))
-  time.sleep(T_MAX)
-  servo.stop()
-  time.sleep(1)
-  set_POS(0)
-  
-  # automated
-  #move(servo,180)
-  #move(servo,90)
-  #init(servo)
-  #time.sleep(2)
-  
-  GPIO.cleanup()
+  move(servo, 0) 
+  move(servo, 180)
+  move(servo, 0)
+
+  PWM.cleanup()
 
 main()
