b7001faec6a0dcc645ac7f1acc25a5b00cc71e58
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2015-08-09 20:01:57 +0200

    Added the target melody.

--- sesame-melody.py
+++ sesame-melody.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+import pyaudio
+
+# configuration
+
+
+# target melody, bar 6 to 13
+target_pitch =
+[
+    "c3", "h2", "f2",                           # bar 06
+    "a2", "g2", "f2", "d2",                     # bar 07
+    "c2", "d2", "g1", "d2",                     # bar 08
+    "e1", "c1", "e1", "g1", "c2", "e2", "g2",   # bar 09
+    "c3", "h2", "f2",                           # bar 10
+    "a2", "g2", "f2", "d2",                     # bar 11
+    "c2", "e2", "d2", "e2",                     # bar 12
+    "d2", "c2"                                  # bar 13
+]
+
+
+
+
 
 def main():
     print "Do nothing serious"
