92ecfa21b35d7545de53e43421ec3413f87ffcdf
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2015-08-10 22:59:50 +0200

    L'écureuil a du mal à se nourrir.

--- sesame-melody.py
+++ sesame-melody.py
@@ -22,27 +22,25 @@ def get_target_pitch():
 
 
 # configure pitch detection
-def create_pitch_alg(pitch_method = "default", pitch_buffer_size = 4*512,
-                     pitch_hop_size = 256, pitch_samplerate = 44100,
-                     pitch_tolerance = 0., pitch_unit = "midi"):
+def create_pitch_alg(pitch_method="default", pitch_buffer_size=4*512,
+                     pitch_hop_size=256, pitch_samplerate=44100,
+                     pitch_tolerance=0.8, pitch_unit="midi"):
     pitch_alg = ab.pitch(pitch_method, pitch_buffer_size, pitch_hop_size, pitch_samplerate)
     pitch_alg.set_tolerance(pitch_tolerance)
     pitch_alg.set_unit(pitch_unit)
     return pitch_alg
 
 # configure onset detection
-def create_onset_alg(onset_method = "default", onset_buffer_size = 512,
-                     onset_hop_size = 256, onset_samplerate = 44100,
-                     onset_threshold = 0.):
+def create_onset_alg(onset_method="default", onset_buffer_size=512,
+                     onset_hop_size=256, onset_samplerate=44100,
+                     onset_threshold=0.):
     onset_alg = ab.onset(onset_method, onset_buffer_size, onset_hop_size, onset_samplerate)
     onset_alg.set_threshold(onset_threshold)
     return onset_alg
 
-def main(args):
+def main(opts):
     print "Do nothing serious"
     # inspired by aubionotes.c
-
-
 
     pitch_alg = create_pitch_alg()
     onset_alg = create_onset_alg()
@@ -50,12 +48,37 @@ def main(args):
     onset_vec = ab.fvec(1)
     pitch_vec = ab.fvec(1)
 
+    source = None
+    if opts.filename is not None:
+        source = ab.source(opts.filename, opts.samplerate, opts.hop_size)
+
     # use median
     use_median = True
     median_size = 6
 
 
 
+    pitches = []
+    confidences = []
+
+    # total number of frames read
+    total_frames = 0
+    while True:
+        samples, read = source()
+        pitch = pitch_alg(samples)[0]
+        confidence = pitch_alg.get_confidence()
+        pitches += [pitch]
+        confidences += [confidence]
+        total_frames += read
+        if read < opts.hop_size: break
+
+    print pitches
+    skip = 1 # skip the first note
+    pitches = np.array(pitches[skip:])
+    confidences = np.array(confidences[skip:])
+    times = [t * opts.hop_size for t in range(len(pitches))]
+
+    print pitches
     return
 
 
@@ -64,8 +87,12 @@ import sys
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Sesame Melody - Note detection")
-    parser.add_argument("-f", "--file", dest ="filename",
+    parser.add_argument("-f", "--file", dest="filename", default=None,
                         help="input file (.wav)", metavar="FILE")
+    parser.add_argument("-R", "--samplerate", dest="samplerate", default=44100,
+                        help="sample rate in Hz", metavar="RATE")
+    parser.add_argument("-H", "--hopsize", dest="hop_size", default=256,
+                        help="hop size in bits", metavar="HOP")
 
     opts = parser.parse_args()
     main(opts)
