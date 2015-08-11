f6a8f8b581dded5e663aa5a5e5ad43b0e38b37d8
Raspberry Pi <pi@raspberry.local>
2015-08-11 17:35:20 +0000

    Analysis

---
+++ pysoundcard-read.py
@@ -0,0 +1,27 @@
+# import pysoundcard as psc
+
+
+# stream = psc.Stream(block_length=16,
+#                     samplerate=44100,
+#                     input_device=True)
+
+# stream.start()
+
+# while True:
+#   stream.read(2048)
+
+
+from pysoundcard import Stream, continue_flag
+import time
+
+"""Loop back five seconds of audio data."""
+
+def callback(in_data, out_data, time_info, status):
+    print "Hey"
+    # out_data[:] = in_data
+    return continue_flag
+
+s = Stream(samplerate=44100, blocksize=16, callback=callback)
+s.start()
+time.sleep(5)
+s.stop()
