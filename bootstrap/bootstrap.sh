658aec976bffd513d5bb7cc5a5ce0e29ded5903e
Raspberry Pi <pi@raspberry.local>
2015-08-11 08:04:34 +0000

    Bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+sudo apt-get update
+sudo apt-get upgrade
 sudo cp etc/modprobe.d/alsa-base.conf /etc/modprobe.d/
 sudo apt-get install $(cat packages  | tr "\n" " ")
+sudo apt-get install python-distutils-extra
+sudo apt-get install python-pyrex
+#sudo pip install python-pyrex
 sudo pip install pyalsa
+sudo pip install pyalsaaudio
 
 git config --global user.email "pi@raspberry.local"
 git config --global user.name "Raspberry Pi"
