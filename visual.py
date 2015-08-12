2f025687ac81c93ab0652ccecba9199519dba2f8
Raspberry Pi <pi@raspberry.local>
2015-08-12 12:48:20 +0000

    new led class: SMLed

---
+++ visual.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+class SMLed:
+
+  def __init__(self, pin_red, pin_green):
+    # RPi.GPIO pin number for red and green led control
+    self.pins = [pin_red, pin_green]
+
+    # initialise GPIO pins and define as output
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(self.pins, GPIO.OUT)
+
+    # switch LED to red
+    GPIO.output(self.pins, (GPIO.HIGH, GPIO.LOW))
+
+  def __del__(self):
+    GPIO.cleanup()
+
+  def red():
+    GPIO.output(self.pins, (GPIO.HIGH, GPIO.LOW))
+
+  def green():
+    GPIO.output(self.pins, (GPIO.LOW, GPIO.HIGH))
+
+  def yellow():
+    GPIO.output(self.pins, (GPIO.HIGH, GPIO.HIGH))
+
+  def off():
+    GPIO.output(self.pins, (GPIO.LOW, GPIO.LOW))
+
+if __name__ == "__main__":
+
+  led = SMLed(15, 16)
+
+  for i in range(10):
+    led.red()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.2)
+    led.green()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.2)
+    led.yellow()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.2)
